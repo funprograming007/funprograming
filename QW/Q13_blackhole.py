b13e8089b8fa6af7e1fa580f6c4762a0463ec04a
FunPrograming <daniel.hpt@outlook.com>
2023-08-23 21:52:02 +0800

    ffsa

--- QW/Q13_blackhole.py
+++ QW/Q13_blackhole.py
@@ -3,5 +3,40 @@
 #举个例子，3位数的黑洞数为495.
 #验证4位数的黑洞数为6174。
 
+# 123   1 2 3 组成  321 - 123 = 198
+# 198   1 9 8 组成  981 - 189 = 792
+# 792   7 9 2 组成  972 - 279 = 693
+# 693   6 9 3 组成  963 - 369 = 594
+# 。。。。。。
+# 495   4 9 5 组成  954 - 459 = 495 黑洞数
+import random
+def convert(number):
+    digitals = list(map(int,str(number)))
+    digitals.sort(reverse=True)
+    maxnum = 0
+    for i in digitals:
+        maxnum=maxnum*10 + i
+
+    digitals.sort()
+    smallnum = 0
+    for i in digitals:
+        smallnum = smallnum*10 + i
+
+    return maxnum - smallnum
+
+init_num = random.randint(1000,9999) # 前后包含的
+
+while(True):
+    print(init_num)
+    j = convert(init_num)
+
+    if init_num == j:
+        print(f"黑洞数是{j}")
+        break
+    else:
+        init_num = j
+
+
+
 
 
