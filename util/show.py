c35cde6134e7d502c3248bae1a71705b104fbb71
FunPrograming <daniel.hpt@outlook.com>
2023-09-05 13:52:19 +0800

    1

---
+++ util/show.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+
+
+class DemoShow:
+    def __init__(self,title):
+        pass
+    def startwin(self):
+        pass
+
+    def start(self):
+        pass
+
+
+    def show(self,matrix,highlight):
+        pass
+
+if __name__ == '__main__':
+    game_win = DemoShow()
+    game_win.start_game()
